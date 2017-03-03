00acaf90e93fc2f933be504924323ce400179aa6
venu <venu@squidera.com>
2017-03-02 23:25:09 -0500

    updating to use spark sql

--- src/main/scala/com/spark/SaleByPerson.scala
+++ src/main/scala/com/spark/SaleByPerson.scala
@@ -35,9 +35,24 @@ object SaleByPerson extends App {
       .option("inferSchema", "true")
       .csv("src/main/resources/data/sales.csv")
 
-    df.select("person").write.format("csv").save("src/main/resources/output/sales")
+    //show the results as output
+    df.select("person", "sale_amount").show(10)
 
-      //df.rdd.collect()
+    //write output to csv
+    //df.select("person").write.format("csv").save("src/main/resources/output/sales")
+
+    // Register the DataFrame as a temporary view
+    df.createOrReplaceTempView("sales")
+
+    // SQL statements can be run by using the sql methods provided by Spark
+    val largeSalesDF = spark.sql("SELECT person, sale_amount FROM sales WHERE sale_amount > 20")
+    // The columns of a row in the result can be accessed by field index
+    largeSalesDF.map(person => "Name: " + person(0)).show()
+
+    //group by
+    val salesGroupedDF = spark.sql("SELECT person, sum(sale_amount) FROM sales group by person")
+    // The columns of a row in the result can be accessed by field index
+    salesGroupedDF.map( person => ("Name: " + person(0) + "\t" + person(1)) ).show()
 
     spark.stop()
   }
